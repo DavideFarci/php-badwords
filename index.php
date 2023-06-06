921d05249289c2d564bfdcd955331bd324e3f73c
DavideFarci <yank92.df@gmail.com>
2023-06-06 17:09:05 +0200

    add exercice

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+    $default_text = 'Il tuo testo è:' ;
+    $user_text = $_GET['text'];
+
+    $censure = $_GET['censure'];
+    $default_censure = 'Il tuo testo censurato è:';
+    $text_censored = str_replace($censure, '***', $user_text) ;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +16,10 @@
     <title>PHP Badwords</title>
 </head>
 <body>
-    
+    <h1><?= $default_text; ?></h1>
+    <p><?= $user_text; ?></p>
+
+    <h2><?= $default_censure; ?></h2>
+    <p><?= $text_censored; ?></p>
 </body>
 </html>
