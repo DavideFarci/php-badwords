802ffcdeb96ab4d1ffde770463a4d3e26a8bbbc6
DavideFarci <yank92.df@gmail.com>
2023-06-07 10:20:02 +0200

    add letters count for text and text_censored

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     $default_censure = 'Il tuo testo censurato è:';
     $censure = $_GET['censure'];
     $text_censored = str_replace($censure, '***', $user_text, $num_censored) ;
+    $letters_count_text = strlen($user_text);
+    $letters_count_censored = strlen($text_censored);
 ?>
 
 <!DOCTYPE html>
@@ -18,9 +20,11 @@
 <body>
     <h1><?= $default_text; ?></h1>
     <p><?= $user_text; ?></p>
+    <p>Il tuo texto ha <?= $letters_count_text ?> lettere</p>
 
     <h2><?= $default_censure; ?></h2>
     <p><?= $text_censored; ?></p>
     <p>Ho censurato la tua parola <?= $num_censored; ?> volta/e</p>
+    <p>Il tuo testo censurato ha <?= $letters_count_censored ?> lettere</p>
 </body>
 </html>
